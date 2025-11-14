bf5e76696275d5fa30d0df20c375513c4e0f53e5
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-11-14 08:38:30 +0600

    Add password reset and forgot password routes
    Introduced new endpoints for user password reset and forgot password flows, including routes for initiating, verifying, and resetting passwords. This enhances the authentication service with password recovery capabilities.

--- apps/auth-service/src/routes/auth.router.ts
+++ apps/auth-service/src/routes/auth.router.ts
@@ -1,19 +1,29 @@
 import express, { Router } from "express";
-import { loginUser, userRegistration, verifyUser } from "../controller/auth.controller";
+import {
+  loginUser,
+  userForgotPassword,
+  userRegistration,
+  verifyUser,
+  resetUserPassword,
+  verifyUserForgotPassword,
+} from "../controller/auth.controller";
 
 const router: Router = express.Router();
 
 // Health check endpoint
 router.get("/health", (req, res) => {
-  res.status(200).json({ 
+  res.status(200).json({
     status: "healthy",
     service: "Auth Service",
-    timestamp: new Date().toISOString()
+    timestamp: new Date().toISOString(),
   });
 });
 
 router.post("/user-registration", userRegistration);
 router.post("/verify-user", verifyUser);
 router.post("/login-user", loginUser);
+router.post("/forgot-password-user", userForgotPassword);
+router.post("/reset-password-user", resetUserPassword);
+router.post("/verify-forgot-password-user", verifyUserForgotPassword);
 
 export default router;
