4716c5b5e6ef501e91e38167ec11d4364f6ac02c
Md. Tarek Rahman <tareksabbir4599@gmail.com>
2025-11-16 20:00:57 +0600

    Create constants.ts

---
+++ apps/user-ui/src/configs/constants.ts
@@ -0,0 +1,34 @@
+export const NavItemsData: NavItemsTypes[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Products",
+    href: "/products",
+  },
+  {
+    title: "Shops",
+    href: "/shops",
+  },
+  {
+    title: "Offers",
+    href: "/offers",
+  },
+  {
+    title: "Become A seller",
+    href: "/become-a-seller",
+  },
+];
+
+
+ export const TopdeaderNavItems = [
+  { name: "About", link: "/about" },
+  { name: "My Account", link: "/my-account" },
+  { name: "Wishlist", link: "/wishlist" },
+  { name: "Orders", link: "/orders" },
+];
